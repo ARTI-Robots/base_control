06dbf2b74954c217129538f266117fb147785561
Alexander Buchegger <alexander.buchegger@student.tugraz.at>
2018-08-24 16:07:55 +0200

    arti_base_control: initialize motors with velocity = 0 / position = 0 to improve handling of offsets

--- arti_base_control/src/motor_factory.cpp
+++ arti_base_control/src/motor_factory.cpp
@@ -19,21 +19,23 @@ MotorFactory::MotorFactory(const ros::NodeHandle& nh, double control_interval, b
 
 DriveMotorPtr MotorFactory::createDriveMotor(ros::NodeHandle& private_nh)
 {
-  const DriveMotorPtr motor = std::make_shared<VescDriveMotor>(private_nh, driver_factory_, control_interval_);
+  DriveMotorPtr motor = std::make_shared<VescDriveMotor>(private_nh, driver_factory_, control_interval_);
   if (publish_motor_states_)
   {
-    return std::make_shared<PublishingDriveMotor>(private_nh, motor);
+    motor = std::make_shared<PublishingDriveMotor>(private_nh, motor);
   }
+  motor->setVelocity(0.0);
   return motor;
 }
 
 SteeringMotorPtr MotorFactory::createSteeringMotor(ros::NodeHandle& private_nh)
 {
-  const SteeringMotorPtr motor = std::make_shared<VescSteeringMotor>(private_nh, driver_factory_, control_interval_);
+  SteeringMotorPtr motor = std::make_shared<VescSteeringMotor>(private_nh, driver_factory_, control_interval_);
   if (publish_motor_states_)
   {
-    return std::make_shared<PublishingSteeringMotor>(private_nh, motor);
+    motor = std::make_shared<PublishingSteeringMotor>(private_nh, motor);
   }
+  motor->setPosition(0.0);
   return motor;
 }
 }
