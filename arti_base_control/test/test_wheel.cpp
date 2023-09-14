f42a1427ddab234f69990eb13d2dd2b4e99f2963
Fabian Hirmann <f.hirmann@arti-robots.com>
2023-09-14 16:09:16 +0200

    fix ROS test of wheel (rewrite to be able to compile again and wrap into rostest such the test can run without any externally started roscore)

--- arti_base_control/test/test_wheel.cpp
+++ arti_base_control/test/test_wheel.cpp
@@ -6,19 +6,30 @@
 #include <memory>
 #include <arti_base_control/steering.h>
 #include <arti_base_control/wheel.h>
+#include <arti_base_control/joint_state.h>
 
 TEST(WheelTest, SimpleFrontWheelTest)
 {
-  const arti_base_control::Wheel wheel(1.0, 0.0, 0.0, 0.5, std::make_shared<arti_base_control::IdealAckermannSteering>(0.0));
-  EXPECT_DOUBLE_EQ(2.0, wheel.computeWheelVelocity(1.0, 0.0, 0.0, 0.0));
-  EXPECT_DOUBLE_EQ(2.0 * std::sqrt(2.0), wheel.computeWheelVelocity(1.0, 1.0, 0.0, 0.0));
-  EXPECT_DOUBLE_EQ(2.0 * std::sqrt(2.0), wheel.computeWheelVelocity(1.0, -1.0, 0.0, 0.0));
-  EXPECT_DOUBLE_EQ(-2.0 * std::sqrt(2.0), wheel.computeWheelVelocity(-1.0, 1.0, 0.0, 0.0));
-  EXPECT_DOUBLE_EQ(-2.0 * std::sqrt(2.0), wheel.computeWheelVelocity(-1.0, -1.0, 0.0, 0.0));
+  const ros::NodeHandle steering_nh("steering");
+  auto ideal_ackermann_steering = std::make_shared<arti_base_control::IdealAckermannSteering>(steering_nh);
+
+  const arti_base_control::Wheel wheel(1.0, 0.0, 0.0, 0.5);
+
+  arti_base_control::JointState expected_steering_state;
+  expected_steering_state.position = 0.0;
+  expected_steering_state.velocity = 0.0;
+  const arti_base_control::JointState wheel_steering_state = ideal_ackermann_steering->computeWheelSteeringState(wheel, expected_steering_state);
+
+  EXPECT_DOUBLE_EQ(2.0, wheel.computeWheelVelocity(1.0, 0.0, wheel_steering_state));
+  EXPECT_DOUBLE_EQ(2.0 * std::sqrt(2.0), wheel.computeWheelVelocity(1.0, 1.0, wheel_steering_state));
+  EXPECT_DOUBLE_EQ(2.0 * std::sqrt(2.0), wheel.computeWheelVelocity(1.0, -1.0, wheel_steering_state));
+  EXPECT_DOUBLE_EQ(-2.0 * std::sqrt(2.0), wheel.computeWheelVelocity(-1.0, 1.0, wheel_steering_state));
+  EXPECT_DOUBLE_EQ(-2.0 * std::sqrt(2.0), wheel.computeWheelVelocity(-1.0, -1.0, wheel_steering_state));
 }
 
 int main(int argc, char** argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
+   ros::init(argc, argv, "steering_tester");
   return RUN_ALL_TESTS();
 }
